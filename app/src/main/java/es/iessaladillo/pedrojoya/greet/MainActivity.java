752bf3acac38742b9afa64228e694d808330c0c7
Andrei Szambati <aszambati@gmail.com>
2020-10-03 16:50:24 +0200

    Modificando Java

--- app/src/main/java/es/iessaladillo/pedrojoya/greet/MainActivity.java
+++ app/src/main/java/es/iessaladillo/pedrojoya/greet/MainActivity.java
@@ -1,6 +1,7 @@
 package es.iessaladillo.pedrojoya.greet;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        TextView texto = requireViewById(R.id.lbltexto);
+
+
     }
 
 
